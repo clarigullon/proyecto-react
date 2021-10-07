0d0c31844d62fd225466179515f28a474609ef1c
clarigullon <clariigullon@gmail.com>
2021-10-07 11:05:45 -0300

    agregando correcciones

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemList from './components/ItemListContainer/ItemList/ItemList';
+
 const App =()=> {
   return (
     <div className="App">
       <header>
         <NavBar/>
         <ItemListContainer greeting="Productos TuttiGlutenFree"/>
-        <ItemList/>
       </header>
      
     </div>
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './ItemListContainer.css'
+import './ItemList/ItemList'
+import ItemList from './ItemList/ItemList'
 
 const ItemListContainer = (props) => {
     return (
-        <div className="ItemListContainer">
-            <h1>{props.greeting}</h1>
+        <div>
+            <div className="ItemListContainer">
+                <h1>{props.greeting}</h1>
+            </div>
+            <ItemList/>
         </div>
     )
 }
